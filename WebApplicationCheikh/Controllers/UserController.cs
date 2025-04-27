771cccd535bf019d1f6438cf9b5503949105929a
minamba camara <minamba.c@gmail.com>
2025-04-28 00:52:20 +0200

    first commit

---
+++ WebApplicationCheikh/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace ApplicationCheikh.Api.Controllers
+{
+    [ApiController]
+    [Route("Users")]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("/users/sendMail")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(string), Description = "Mail de contact")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "An unexpected error occurred")]
+        public async Task<IActionResult> SendContactMailAsync()
+        {
+            return Ok();
+        }
+    }
+}
